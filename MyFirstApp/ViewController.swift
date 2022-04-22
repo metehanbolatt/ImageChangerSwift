f149356fc1065ddfce5b3a2e45ec4ecd175868e9
metehanie <metehan.bolat.ie@gmail.com>
2022-04-23 01:44:24 +0300

    Filled button was added.

--- MyFirstApp/ViewController.swift
+++ MyFirstApp/ViewController.swift
@@ -9,11 +9,23 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var myLabel: UILabel!
+    @IBOutlet weak var imageView: UIImageView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
 
+    @IBAction func filledButtonClicked(_ sender: Any) {
+        
+        imageView.image = UIImage(named: "buzDevriTwo")
+    }
+    
+    @IBAction func changeButtonClicked(_ sender: Any) {
+        
+        imageView.image = UIImage(named: "buzDevri")
+    }
 }
 
